eb0e885f189322ae79675a1934228afa675557e9
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-02-14 16:41:50 -0500

    Adding answers

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -2,16 +2,20 @@
   (:refer-clojure :exclude [boolean]))
 
 (defn boolean [x]
-  ":(")
+  (if (or (= x nil) (= x false)) false true))
 
 (defn abs [x]
-  ":(")
+  (if (neg? x) (+ x (* x -2)) x))
 
 (defn divides? [divisor n]
-  ":(")
+  (if (= (mod n divisor) 0) true false))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond 
+    (= (divides? n 3) 0) "fizz"
+    (= (divides n 5) 0) "buzz"
+    (= (divides n 15) 0) "gotcha!"
+    :else ""))    
 
 (defn teen? [age]
   ":(")
